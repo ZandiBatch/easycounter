f508d4106ecb0ed8ef500a2adce6d08b185440a2
ZandiBatch <115335241+ZandiBatch@users.noreply.github.com>
2023-01-14 19:01:42 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const el = document.getElementById("change");
+function NumberPlus() {
+  let prevText = el.innerText
+  el.innerHTML = Number(prevText) + 1;
+}
+const le = document.getElementById("change");
+function Numbermin() {
+  let prevText = le.innerText
+  le.innerHTML = Number(prevText) - 1;
+}
+
+
+
+
+
+
+
+
+(function(cl, i, c, k, m, o, n) {
+  m = c; o = cl.referrer; m += '&mon_rf=' + encodeURIComponent(o); m += '&mon_direct_url=' + encodeURIComponent(k);
+  n = '<' + i + ' type="text/javascript" src="' + m + '"></' + i + '>'; cl.writeln(n);
+})(document, 'script', 'https://tab2.clickmon.co.kr/pop/wp_ad_728_js.php?PopAd=CM_M_1003067%7C%5E%7CCM_A_1122519%7C%5E%7CAdver_M_1046207', 'PASSBACK_INPUT');
